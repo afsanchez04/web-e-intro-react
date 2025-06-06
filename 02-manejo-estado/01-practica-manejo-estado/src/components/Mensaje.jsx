b85d06ffc7e05604297256fe21d4a1bfcebc4145
afsaanchez04 <afsanchez04@gmail.com>
2025-06-05 21:50:37 -0500

    02-manejo-estado

---
+++ 02-manejo-estado/01-practica-manejo-estado/src/components/Mensaje.jsx
@@ -0,0 +1,24 @@
+
+function Mensaje({texto, color}) {
+
+  return <h2>{texto}</h2>;
+
+}
+
+export default Mensaje;
+
+/* function Mensaje(props) {
+
+    const props = {
+        texto: "Holamundo",
+        color: "red",
+        pais: "colombia"
+    }
+
+  return <h2>{props.texto}</h2>;
+
+} */
+
+
+
+
