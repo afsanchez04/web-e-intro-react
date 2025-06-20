2b2a0abb253815684db673a8913434f37b13b3fd
afsaanchez04 <afsanchez04@gmail.com>
2025-06-19 21:39:17 -0500

    useReducer

--- 06-hooks-avanzados-react/01-practica-clase/src/App.jsx
+++ 06-hooks-avanzados-react/01-practica-clase/src/App.jsx
@@ -1,4 +1,6 @@
+import { Contador } from "./components/Contador"
 import { ContadorConCallback } from "./components/ContadorConCallback"
+import { Formulario } from "./components/Formulario"
 import { FormularioComponent } from "./components/FormularioComponent"
 import { Padre } from "./components/Padre"
 
@@ -6,7 +8,7 @@ import { Padre } from "./components/Padre"
 export const App = () => {
   return (
     <>
-      <Padre />
+      <Formulario />
     </>
   )
 }
