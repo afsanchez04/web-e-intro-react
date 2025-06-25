27d4874340d9110a67752e57d5d37a1578f2d755
afsaanchez04 <afsanchez04@gmail.com>
2025-06-24 20:41:02 -0500

    navegación rutas

--- 07-manejo-rutas/01-practica/src/App.jsx
+++ 07-manejo-rutas/01-practica/src/App.jsx
@@ -1,8 +1,22 @@
+import { Route, Routes } from "react-router-dom"
 import { NavBar } from "./components/NavBar"
+import { Home } from "./Pages/Home"
+import { AboutUs } from "./Pages/AboutUs"
+import { Store } from "./Pages/Store"
+import { Cart } from "./Pages/Cart"
 
 
 export const App = () => {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home/>}/> {/* Home */}
+        <Route path="/about" element={<AboutUs/>}/>
+        <Route path="/store" element={<Store/>}/>
+        <Route path="/carrito" element={<Cart/>}/>
+      </Routes>
+    </>
+
   )
 }
--- 07-manejo-rutas/01-practica/src/components/NavBar.jsx
+++ 07-manejo-rutas/01-practica/src/components/NavBar.jsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom"
 
 
 export const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-sm bg-body-tertiary">
             <div className="container">
                 <a className="navbar-brand" href="#">
                     Navbar
@@ -21,14 +22,28 @@ export const NavBar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">
+                            {/* <a className="nav-link active" aria-current="page" href="#">
                                 Home
-                            </a>
+                            </a> */}
+                            <Link className="nav-link active" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Link
-                            </a>
+                            {/* <a className="nav-link" href="#">
+                                AboutUs
+                            </a> */}
+                            <Link className="nav-link" to="/about">AboutUs</Link>
+                        </li>
+                        <li className="nav-item">
+                            {/* <a className="nav-link" href="#">
+                                Store
+                            </a> */}
+                            <Link className="nav-link" to="/store">Store</Link>
+                        </li>
+                        <li className="nav-item">
+                           {/*  <a className="nav-link" href="#">
+                                Cart
+                            </a> */}
+                            <Link className="nav-link" to="/carrito">Cart</Link>
                         </li>
                     </ul>
                 </div>
